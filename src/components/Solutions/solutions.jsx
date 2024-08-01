36b2a738e5d4238e992dc207e6b91d89487620a0
MuraliMantha <muralikrishnam056@gmail.com>
2024-08-01 22:44:35 +0530

    first commit

---
+++ src/components/Solutions/solutions.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const solutionsArray = [
+    {
+        
+    }
+]
+
+const Solutions = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Solutions
